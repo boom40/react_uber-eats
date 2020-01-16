37f0bd9537715fe264fffefb09563cb6685768ee
boom40 <kickgames675@gmail.com>
2020-01-16 03:24:57 +0200

    Updates

---
+++ precache-manifest.b71b626b763ed344fe746a3d1f887478.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eacd618dbf5a10aff53b966d0341c39a",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "7fe20240b4a6eb0aad5e",
+    "url": "/react_uber-eats/static/css/main.6bb0058d.chunk.css"
+  },
+  {
+    "revision": "5f4d42c3820722ea433c",
+    "url": "/react_uber-eats/static/js/2.3689a0b9.chunk.js"
+  },
+  {
+    "revision": "7fe20240b4a6eb0aad5e",
+    "url": "/react_uber-eats/static/js/main.eb2caaf4.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
